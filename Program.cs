1b4db9b4493d75915356d7a6cf94ccb2e8b7ca27
Adam <tdp934@alumni.ku.dk>
2026-02-24 16:05:40 +0100

    Made the program display the creation and sorting of two hands

--- Program.cs
+++ Program.cs
@@ -2,6 +2,23 @@ namespace Whist;
 class Program{
     static void Main() {
         Deck deck = new Deck();
-        deck.PrintDeck();
+            deck.Shuffle();
+
+            Player player1 = new Player("Alice");
+            Player player2 = new Player("Bob");
+
+            player1.CreateHand(deck);
+            player2.CreateHand(deck);
+
+            player1.HandSort();
+            player2.HandSort();
+
+            player1.PrintHand();
+            Console.WriteLine();
+            player2.PrintHand();
+
+            Console.WriteLine($"\nCards left in deck: {deck.Cards.Count}");
     }
+
+    
 }
