96e36e8d5870b1467a13ae296c842cad79782877
Adam <tdp934@alumni.ku.dk>
2026-02-24 16:05:24 +0100

    Added Player.cs with Hand building and sorting

---
+++ Player.cs
@@ -0,0 +1,27 @@
+namespace Whist;
+
+public class Player {
+    public string Name { get; set; }
+    public List<Card> Hand { get; private set; }
+
+    public Player(string name) {
+        Name = name;
+        Hand = new List<Card>();
+    }
+
+    public void CreateHand(Deck deck) {
+        Hand.AddRange(deck.Cards.GetRange(0, 13));
+        deck.Cards.RemoveRange(0, 13);
+    }
+
+    public void PrintHand() {
+            Console.WriteLine($"{Name}'s hand:");
+            foreach (var card in Hand) {
+                Console.WriteLine(card);
+            }
+    }
+
+    public void HandSort() {
+        Hand.Sort();
+    }
+}
